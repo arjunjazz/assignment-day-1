43a8c86948f79d3b5c29fce1bf1a7a774b451f1b
arjunjazz <78622303+arjunjazz@users.noreply.github.com>
2021-02-06 04:16:23 +0530

    Day 1 assignments

---
+++ DAY 1/day 1 Q.1.py
@@ -0,0 +1,7 @@
+digit_1 = int(input("enter a number:  "))
+digit_2 = int(input("enter a number:  "))
+
+print("\nAddition Result = ", digit_1+digit_2)
+print("Subtraction Result = ", digit_1-digit_2)
+print("Multiplication Result = ", digit_1*digit_2)
+print("Division Result = ", digit_1/digit_2)
---
+++ DAY 1/day 1 Q.2.py
@@ -0,0 +1,4 @@
+x = int(input("entre a number:  "))
+y = int(input("entre a number:  "))
+
+print("\nx to the power of y =  " , x**y)
